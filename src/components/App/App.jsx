48eb020a0d83f30eab8752cbadb1245f2147155d
Ian J.Um <31336310+code0xff@users.noreply.github.com>
2020-03-13 00:02:46 +0900

    add get information

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,11 +1,37 @@
 import React, {Component} from 'react'
 
 class App extends Component {
-  render () {
+  state={stores: []}
+
+  componentDidMount() {
+    navigator.geolocation.getCurrentPosition((pos) => {
+      const latitude = pos.coords.latitude
+      const longitude = pos.coords.longitude
+
+      const stores = []
+      fetch(`https://8oi9s0nnth.apigw.ntruss.com/corona19-masks/v1/storesByGeo/json?lat=${latitude}&lng=${longitude}&m=500`)
+        .then(response => response.json())
+        .then(jsonObject => {
+          jsonObject.stores.forEach(element => {
+            stores.push(element)
+          })
+          this.setState({stores})
+          console.log(this.state.stores)
+        })
+    })
+  }
+
+  render() {
+    const {stores} = this.state
     return (
-      <div className='App'>
-        Test
+      <div className='app'>
+        {
+          stores.map((store, key) => {
+            return (<p key={key}>{store.name}</p>)
+          })
+        }
       </div>
+
     )
   }
 }
