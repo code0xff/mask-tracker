a518a98c5749d916bad9c3d7b56000793b552697
Ian J.Um <31336310+code0xff@users.noreply.github.com>
2020-03-14 16:15:30 +0900

    Updates

---
+++ precache-manifest.de13236cc57b853848191a28b87a7eb1.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0af72f9463dd3ad7c2509d4c3dff7bfb",
+    "url": "/mask-tracker/index.html"
+  },
+  {
+    "revision": "3378af8ae20a1deaeafe",
+    "url": "/mask-tracker/static/css/2.47e06e2e.chunk.css"
+  },
+  {
+    "revision": "f727493d41dac92d7ab8",
+    "url": "/mask-tracker/static/css/main.6ede84e1.chunk.css"
+  },
+  {
+    "revision": "3378af8ae20a1deaeafe",
+    "url": "/mask-tracker/static/js/2.6ce00c61.chunk.js"
+  },
+  {
+    "revision": "11a928b57aca5049fe950bf8bcde77c5",
+    "url": "/mask-tracker/static/js/2.6ce00c61.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f727493d41dac92d7ab8",
+    "url": "/mask-tracker/static/js/main.779f0dd2.chunk.js"
+  },
+  {
+    "revision": "8198b6822b3636633a26",
+    "url": "/mask-tracker/static/js/runtime-main.53775d6a.js"
+  }
+]);
